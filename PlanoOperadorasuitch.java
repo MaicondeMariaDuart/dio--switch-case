5ec9248f25c64b011fe1c6d6da97c73623cd26f0
MaicondeMariaDuart <156396043+MaicondeMariaDuart@users.noreply.github.com>
2024-01-19 01:38:29 -0300

    Add files via upload

---
+++ PlanoOperadorasuitch.java
@@ -0,0 +1,22 @@
+public class PlanoOperadorasuitch {
+    public static void main (String[] args){
+        String plano = "M";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5GB de Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instragam gratis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
